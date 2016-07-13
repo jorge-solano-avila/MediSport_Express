9430d781c3e136dd81d38f3a20539e6d4012a21d
jorge52an <jasolanoa@unal.edu.co>
2016-07-13 16:32:39 -0500

    feat: add post and get centers

--- src/server.js
+++ src/server.js
@@ -7,6 +7,24 @@ var app = express();
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
+var rad = function( x )
+{
+	return x * Math.PI / 180;
+}
+
+var getDistance = function( p1, p2 )
+{
+	var R = 6378137;
+	var dLat = rad( p2[0] - p1[0] );
+	var dLong = rad( p2[1] - p1[1] );
+	var a = Math.sin( dLat / 2 ) * Math.sin( dLat / 2 ) +
+        Math.cos( rad( p1[0] ) ) * Math.cos( rad( p2[0] ) ) *
+        Math.sin( dLong / 2 ) * Math.sin( dLong / 2 );
+    var c = 2 * Math.atan2( Math.sqrt( a ), Math.sqrt( 1 - a ) );
+    var d = R * c;
+    return d;
+}
+
 app.post( "/user", function( request, response )
 {
     User.findOne( { "username": request.body.username }, function( error, user )
@@ -47,8 +65,31 @@ app.get( "/centers", function( request, response )
     {
         if( error )
             response.send( error );
+        else
+            response.json( centers );
+    } );
+} );
 
-        response.json( centers );
+app.post( "/centers", function( request, response )
+{
+    Center.find( function( error, centers )
+    {
+        if( error )
+            response.send( error );
+        else
+        {
+            var nearbyCenters = [];
+            for( var i in centers )
+            {
+                var position = [request.body.latitude, request.body.longitude];
+                var center = [centers[i].latitude, centers[i].longitude];
+                var distance = getDistance( position, center );
+                console.log( distance );
+                if( distance < request.body.value )
+                    nearbyCenters.push( centers[i] )
+            }
+            response.json( nearbyCenters );
+        }
     } );
 } );
 
